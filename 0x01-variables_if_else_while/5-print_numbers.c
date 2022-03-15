72d99237eff7eef92a0fc149bdad74fb6e61530b
henroy99 <nwaezetochukwu77@gmail.com>
2022-03-14 20:17:30 -0700

    My commit on the 5th task

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Prints numbers from 0 to 9.
  *
@@ -7,13 +6,12 @@
  */
 int main(void)
 {
- int i;
+int i;
 
 for (i = 0; i < 10; i++)
- {
-  printf("%d", i);
- }
- putchar('\n');
- return (0);
+{
+printf("%d", i);
 }
-
+putchar('\n');
+return (0);
+}
